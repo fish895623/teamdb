6b512bed4028f13ef8a76c1b2e770f69b08bf404
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-05 21:17:03 +0900

    feat: Add slf4j logging and read properties from file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,32 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 
 public class Main {
+  private static final Logger log = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
+    Properties properties = new Properties();
+
+    try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+      properties.load(input);
+    } catch (IOException e) {
+      log.warn(e.getMessage());
+    }
+
+    log.info("Application Name: {}", properties.getProperty("hello"));
+
     Frame frame = new Frame("AWT Example");
 
     frame.setSize(300, 200);
