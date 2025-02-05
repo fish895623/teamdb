992a4ed746ff9e60bef2d67ba786aab04dcbbbbf
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-05 21:30:49 +0900

    feat: Add DatabaseManager class

---
+++ src/main/java/org/example/DatabaseManager.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class DatabaseManager {
+  private static DatabaseManager instance;
+
+  private DatabaseManager() {
+  }
+
+  public static synchronized DatabaseManager getInstance() {
+    if (instance == null) {
+      instance = new DatabaseManager();
+    }
+    return instance;
+  }
+
+}
