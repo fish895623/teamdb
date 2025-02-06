db79a1dbea22794e2637185c9e6c77501e76a8b2
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-06 20:10:22 +0900

    feat: Update App class to enhance UI and keyboard event handling

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import javax.swing.text.JTextComponent;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.SQLException;
@@ -16,10 +15,11 @@ import java.util.List;
 
 public class App extends Frame {
   private static final Logger log = LoggerFactory.getLogger(App.class);
+  Button button = new Button("New Hospital");
 
   public App() throws SQLException {
     super("Member Management");
-    setSize(300, 200);
+    setSize(600, 400);
     addWindowListener(new WindowAdapter() {
       public void windowClosing(WindowEvent windowEvent) {
         System.exit(0);
@@ -48,8 +48,10 @@ public class App extends Frame {
           return false;
         });
 
-    Button button = new Button("Click Me");
     add(button, BorderLayout.SOUTH);
+    button.addActionListener(e -> {
+      log.info("Button clicked");
+    });
 
     List<Hospital> users = new HospitalDB().getAll();
 
