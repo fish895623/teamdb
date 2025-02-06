c18401ef742fd600a29988f80681adb9ddd41d12
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-06 11:23:22 +0900

    feat: Add database migration functionality

--- src/main/java/org/example/DatabaseManager.java
+++ src/main/java/org/example/DatabaseManager.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class DatabaseManager {
+  private static final Logger log = LoggerFactory.getLogger(DatabaseManager.class);
   private static DatabaseManager instance;
+
+  static {
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   private DatabaseManager() {
   }
@@ -13,4 +37,78 @@ public class DatabaseManager {
     return instance;
   }
 
+  /**
+   * Auto migrate database inside resources/migration natively not using any library
+   */
+  static void migrate() throws IOException {
+    DatabaseManager.createMigrationTable();
+
+    Connection conn;
+    try {
+      conn = DatabaseManager.getInstance().getConnection();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
+    try {
+      for (Path path : getMigrationFiles()) {
+        String sql = Files.readString(path);
+
+        // if migration already executed, skip
+        if (conn.createStatement()
+            .executeQuery("SELECT * FROM migration WHERE name = '" + path.getFileName() + "'")
+            .next()) {
+          log.info("Migration already executed, skipping");
+        } else {
+          log.info("Executing migration: {}", path.getFileName());
+
+          // execute migration
+          conn.createStatement().executeUpdate(sql);
+          // insert migration record
+          conn.createStatement()
+              .executeUpdate("INSERT INTO migration (name) VALUES ('" + path.getFileName() + "')");
+        }
+      }
+    } catch (IOException | SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Create migration table if not exists
+   *
+   * @return boolean
+   * @throws IOException
+   */
+  public static boolean createMigrationTable() throws IOException {
+    try (Connection conn = DatabaseManager.getInstance().getConnection()) {
+      String sql =
+          "CREATE TABLE IF NOT EXISTS migration (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL)";
+      return conn.createStatement().execute(sql);
+    } catch (SQLException e) {
+      throw new IOException("Failed to create migration table", e);
+    }
+  }
+
+  /**
+   * Get all migration files by human order
+   *
+   */
+  public static List<Path> getMigrationFiles() throws IOException {
+    try (Stream<Path> paths = Files.walk(
+        Paths.get(DatabaseManager.class.getClassLoader().getResource("migration").toURI()))) {
+      return paths.filter(Files::isRegularFile).collect(Collectors.toList());
+    } catch (URISyntaxException e) {
+      throw new IOException("Invalid URI syntax", e);
+    }
+  }
+
+  public Connection getConnection() throws SQLException {
+    Connection connection = null;
+    String url = Prop.get("db.url") + ":" + Prop.get("db.port") + "/" + Prop.get(
+        "db.name") + "?allowMultiQueries=true";
+    connection = DriverManager.getConnection(url, Prop.get("db.user"), Prop.get("db.password"));
+
+    return connection;
+  }
 }
---
+++ src/main/resources/migration/20240206_103815.sql
@@ -0,0 +1,67 @@
+CREATE TABLE IF NOT EXISTS User
+(
+    UserID        INT PRIMARY KEY,
+    Name          VARCHAR(50)  NOT NULL,
+    BirthDate     DATE,
+    Gender        CHAR(1),
+    ContactNumber VARCHAR(20),
+    Password      VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS HealthData
+(
+    HealthDataID           INT PRIMARY KEY,
+    UserID                 INT,
+    MeasurementDateTime    DATETIME,
+    Height                 FLOAT,
+    Weight                 FLOAT,
+    BodyFatPercentage      FLOAT,
+    BloodPressureSystolic  INT,
+    BloodPressureDiastolic INT,
+    BloodSugar             FLOAT,
+    HeartRate              INT,
+    StressLevel            INT,
+    FOREIGN KEY (UserID) REFERENCES User (UserID)
+);
+
+CREATE TABLE IF NOT EXISTS Exercise
+(
+    ExerciseID       INT PRIMARY KEY,
+    UserID           INT,
+    ExerciseDateTime DATETIME,
+    ExerciseType     VARCHAR(50),
+    Duration         INT,
+    CaloriesBurned   INT,
+    FOREIGN KEY (UserID) REFERENCES User (UserID)
+);
+
+CREATE TABLE IF NOT EXISTS Diet
+(
+    DietID       INT PRIMARY KEY,
+    UserID       INT,
+    MealDateTime DATETIME,
+    FoodName     VARCHAR(100),
+    Quantity     FLOAT,
+    Calories     INT,
+    FOREIGN KEY (UserID) REFERENCES User (UserID)
+);
+
+CREATE TABLE IF NOT EXISTS Hospital
+(
+    HospitalID    INT PRIMARY KEY,
+    HospitalName  VARCHAR(100) NOT NULL,
+    Address       VARCHAR(255),
+    ContactNumber VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS MedicalRecord
+(
+    MedicalRecordID INT PRIMARY KEY,
+    UserID          INT,
+    HospitalID      INT,
+    VisitDateTime   DATETIME,
+    Diagnosis       TEXT,
+    Prescription    TEXT,
+    FOREIGN KEY (UserID) REFERENCES User (UserID),
+    FOREIGN KEY (HospitalID) REFERENCES Hospital (HospitalID)
+);
