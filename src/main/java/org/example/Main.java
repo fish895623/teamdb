d45987f95bffc2bc453199c9e78a4f89cc2ce672
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-06 19:56:38 +0900

    style: Remove unused imports from Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,6 @@ package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.sql.SQLException;
 
