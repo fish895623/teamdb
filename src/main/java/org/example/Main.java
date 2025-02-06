b36a42c39569c39c05182635e13b693b34f78b82
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-06 11:31:46 +0900

    feat: add database migration to main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,12 +8,19 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
+import java.io.IOException;
 
 public class Main {
   private static final Logger log = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) {
+    try {
+      DatabaseManager.migrate();
+    } catch (IOException e) {
+      log.error("databaes migration failed", e);
+      return;
+    }
+
     Frame frame = new Frame("AWT Example");
 
     frame.setSize(300, 200);
