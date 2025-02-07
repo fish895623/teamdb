5879942013eea42b4a5b9cf1db8f86ea7f58c85b
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-07 14:53:16 +0900

    WIP

--- src/main/java/org/example/widgets/UserMain.java
+++ src/main/java/org/example/widgets/UserMain.java
@@ -73,6 +73,8 @@ public class UserMain extends JFrame {
     JTable table = getJTable(users);
     JScrollPane scrollPane = new JScrollPane(table);
     add(scrollPane, BorderLayout.CENTER);
+
+    setVisible(true);
   }
 
   public static UserMain getInstance() {
